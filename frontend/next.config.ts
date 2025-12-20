a2b71fc75604e57e16e01cc4a16b7bb05be2590d
StaubDév <guillaume@staub.pro>
2025-12-20 15:55:32 +0100

    Enhance logging and image config for services
    Added logging options and environment variables to the Directus service in compose.yml, including log level, style, websockets, and activity retention. Configured frontend Next.js to allow remote images from specific hosts and set images to be unoptimized.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "superflavio-api.local",
+        pathname: "/assets/*",
+      },
+      {
+        protocol: "http",
+        hostname: "directus",
+        port: "8055",
+        pathname: "/assets/*",
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
